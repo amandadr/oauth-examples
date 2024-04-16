abf1877a82c55d88193e61125f3ecb98d8b9da9f
amandadr <amandadroy@gmail.com>
2024-04-16 14:08:37 -0300

    running examples for cli oauth flow in docs and gsc

---
+++ search-console/client_secrets.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "installed": {
+    "client_id": '[client id]',
+    "client_secret": "[client secret]",
+    "redirect_uris": [],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token"
+  }
+}
